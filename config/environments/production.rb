bcff41bb3b99a4c8ca880cd1c9accca119043561
yoko <yoko.hashimoto.29@gmail.com>
2019-01-19 20:45:32 +0900

    app/config/environments/production.rbを修正した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'photo--share' }
+  config.action_mailer.default_url_options = { host: 'photo--share.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
